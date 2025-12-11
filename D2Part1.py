890da7f6b9507c3d22491464ebd290e520094b0e
Prateek Priyanshu <work.prateekpriyanshu@gmail.com>
2025-12-11 18:17:08 +0530

    done with different loops, range statement, break, continue and pass statements

--- D2Part1.py
+++ D2Part1.py
@@ -1,80 +1,80 @@
-# # different printing methods
-# print("Hellow")
-# name = "ryu"
-# age = 21
-# # multiple items
-# print("name", name, "age", age) # , automatically inserts a space
-# # f-strings
-# print(f"name is {name} and age is {age}")
+# different printing methods
+print("hello")
+name = "ryu"
+age = 21
+# multiple items
+print("name", name, "age", age) # , automatically inserts a space
+# f-strings
+print(f"name is {name} and age is {age}")
 
 
-# # writing to a file - replaces all the text if any present
-# with open("test.txt", "w") as file:
-#     file.write("chicken tandoori1\n")
-#     file.write("chicken tandoori2\n")
-#     file.write("chicken tandoori3\n")
-#     file.write("chicken tandoori4\n")
-#     file.write("chicken tandoori5\n")
-#     file.write("chicken tandoori6\n")
-#     file.write("chicken tandoori7\n")
-#     file.write("chicken tandoori8\n")
-#     file.write("chicken tandoori9\n")
-#     file.write("chicken tandoori10\n")
-# # reading from a file
-# with open("test.txt", "r") as file:
-#     data = file.read()
-# print(data)
-# # reading line by line
-# with open("test.txt", "r") as file:
-#     line1 = file.readline()
-#     line2 = file.readline()
-# print(line1)
-# print(line2)
-# # output
-# # chicken tandoori1
-# # chicken tandoori2
+# writing to a file - replaces all the text if any present
+with open("test.txt", "w") as file:
+    file.write("chicken tandoori1\n")
+    file.write("chicken tandoori2\n")
+    file.write("chicken tandoori3\n")
+    file.write("chicken tandoori4\n")
+    file.write("chicken tandoori5\n")
+    file.write("chicken tandoori6\n")
+    file.write("chicken tandoori7\n")
+    file.write("chicken tandoori8\n")
+    file.write("chicken tandoori9\n")
+    file.write("chicken tandoori10\n")
+# reading from a file
+with open("test.txt", "r") as file:
+    data = file.read()
+print(data)
+# reading line by line
+with open("test.txt", "r") as file:
+    line1 = file.readline()
+    line2 = file.readline()
+print(line1)
+print(line2)
+# output
+# chicken tandoori1
+# chicken tandoori2
 
-# # reading all the lines as list
-# with open("test.txt", "r") as file:
-#     lines = file.readlines()
-# print(lines)
-# # ['chicken tandoori1\n', 'chicken tandoori2\n', 'chicken tandoori3\n', 'chicken tandoori4\n', 'chicken tandoori5\n', 'chicken tandoori6\n', 'chicken tandoori7\n', 'chicken tandoori8\n', 'chicken tandoori9\n', 'chicken tandoori10\n']
+# reading all the lines as list
+with open("test.txt", "r") as file:
+    lines = file.readlines()
+print(lines)
+# ['chicken tandoori1\n', 'chicken tandoori2\n', 'chicken tandoori3\n', 'chicken tandoori4\n', 'chicken tandoori5\n', 'chicken tandoori6\n', 'chicken tandoori7\n', 'chicken tandoori8\n', 'chicken tandoori9\n', 'chicken tandoori10\n']
 
 
-# # operators in python
-# # arithmetic operators
-# print(10+5)
-# print(10-5)
-# print(10*5)
-# print(10/5)
-# print(10**5) # exponential
-# print(10//6) # floor
-# # comparison operators
-# print(10==5)
-# print(10!=5)
-# print(10>5)
-# print(10>=5)
-# print(10<=5)
-# print(10<5)
-# # logical operators
-# print(True and False)
-# print(True or False)
-# print(not True)
-# # assignment operators
-# a = 90
-# a += 10
-# a -= 10
-# a *= 10
-# a /= 10
-# a **= 10
-# a //= 10
-# # membership operators
-# fruits = ["apple", "banana", "cherry", "dragonfruit"]
-# print("apple" in fruits)
-# print("blueberry" in fruits)
-# # identity operators
-# x = 9
-# y = 8
-# print(x is 9)
-# print(x is y)
-# print(y is 8)
+# operators in python
+# arithmetic operators
+print(10+5)
+print(10-5)
+print(10*5)
+print(10/5)
+print(10**5) # exponential
+print(10//6) # floor
+# comparison operators
+print(10==5)
+print(10!=5)
+print(10>5)
+print(10>=5)
+print(10<=5)
+print(10<5)
+# logical operators
+print(True and False)
+print(True or False)
+print(not True)
+# assignment operators
+a = 90
+a += 10
+a -= 10
+a *= 10
+a /= 10
+a **= 10
+a //= 10
+# membership operators
+fruits = ["apple", "banana", "cherry", "dragonfruit"]
+print("apple" in fruits)
+print("blueberry" in fruits)
+# identity operators
+x = 9
+y = 8
+print(x is 9)
+print(x is y)
+print(y is 8)
---
+++ D3Part1.py
@@ -0,0 +1,61 @@
+# # if - elif - else loop
+# a = 12
+# if (a % 6 == 0):
+#     print("a is divided by 6")
+# elif (a % 3 == 0):
+#     print("a is divided by 3")
+# else:
+#     print("a is divided by 2")
+
+# # match case is introduced in Python 3.10 for matching statement
+# b = 333
+# match b:
+#     case 100:
+#         print("fail")
+
+#     case 200:
+#         print("fail")
+
+#     case 333:
+#         print("success")
+
+#     case _:
+#         print("Default case - unknown status")
+
+# # for - loops
+# fruits = ["apple", "banana", "coconut", "dragon fruit"]
+# for item in fruits:
+#     print(item, end=" ")  # this helps in printing in one line
+#     print(item)
+
+# # range loops
+# for num in range(1, 6):
+#     # this will print from start to end-1
+#     print(num)
+
+# # while loops
+# for num in range(1, 7):
+#     if (num % 2 == 0):
+#         print(num)
+
+# # infinite loops - be careful of them
+# while True:
+#     print("helu")
+
+# # break - used to exit a loop
+# for x in range(1,7):
+#     if (x%6==0):
+#         break
+# # continue - skips the current loop if true
+# for x in range(1,7):
+#     if(x%2==0):
+#         continue
+#     else:
+#         print(x)
+# # pass - placeholder - does nothing
+# for i in range(1,8):
+#     if(i%3==0):
+#         pass
+#     print(i)
+
+
